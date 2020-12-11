dd058bff660936baac8abcf29c87626b4ac690d9
uimarshall <uimarshall@gmail.com>
2020-12-11 16:35:27 +0100

    Add controls for user to move left-right

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -15,6 +15,7 @@ class GameScene extends Phaser.Scene {
     this.DEPTH = {
       floor: 0,
       player: 1,
+      ui:2
     };
 
     this.generator = new Generator(this);
@@ -23,6 +24,13 @@ class GameScene extends Phaser.Scene {
     this.allowInput = false;
     this.isPause = false;
     this.isGameover = false;
+
+    // Controls
+    this.isHolding={
+      left:false,
+      right:false,
+      direction:false,
+    }
 
     // Camera
     this.camSpeed = {
@@ -39,15 +47,21 @@ class GameScene extends Phaser.Scene {
 
 
   create() {
-    // this.text = this.add.text(this.CONFIG.centerX, this.CONFIG.centerY, 'Menu');
-    // this.text.setColor('#ffffff');
-    // this.text.setOrigin(0.5);
+    
 
     // Craete floor
     this.generator.setup();
 
     // Player
     this.createPlayer();
+
+    // Controls
+    this.createControls()
+
+    // Start the game
+    this.allowInput=true
+    this.isPause=false
+    this.isGameover=false
   }
 
   update() {
@@ -61,6 +75,17 @@ class GameScene extends Phaser.Scene {
 
     // Move player downward
     this.player.setSpritesPos(this.player.x, this.player.y + this.camSpeed.current);
+
+    // Move player sideways
+    if (this.isHolding.direction==='left') {
+       this.player.setSpritesPos(this.player.x-1, this.player.y);
+
+      
+    }else if(this.isHolding.direction==='right'){
+       this.player.setSpritesPos(this.player.x+1, this.player.y);
+
+
+    }
   }
 
   // Player Sprite
@@ -95,6 +120,74 @@ class GameScene extends Phaser.Scene {
       this.camSpeed.current,
       0,
     );
+  }
+
+  createControls(){
+    // Create zones for input
+    let w = 0.45 * this.CONFIG.width
+    let h = this.CONFIG.height
+
+    this.zoneLeft = this.add.zone(0,0,w,h)
+    this.zoneLeft.setOrigin(0,0)
+    this.zoneLeft.setDepth(this.DEPTH.ui)
+    this.zoneLeft.setScrollFactor(0)
+
+    this.zoneRight = this.add.zone(this.CONFIG.width,0,w,h)
+    this.zoneRight.setOrigin(1,0)
+    this.zoneRight.setDepth(this.DEPTH.ui)
+    this.zoneRight.setScrollFactor(0)
+
+    // let debug = this.add.graphics({x:0,y:0})
+    // debug.fillStyle('0x000000',0.5)
+    // debug.fillRect(0,0,w,h)
+    // debug.setScrollFactor(0)
+    // debug.setDepth(this.DEPTH.ui)
+
+    // Add input callbacks
+    this.zoneLeft.setInteractive()
+    this.zoneLeft.on('pointerdown',this.holdLeft,this)
+    this.zoneLeft.on('pointerup',this.releaseLeft,this)
+    this.zoneLeft.on('pointerout',this.releaseLeft,this)
+
+    this.zoneRight.setInteractive()
+    this.zoneRight.on('pointerdown',this.holdRight,this)
+    this.zoneRight.on('pointerup',this.releaseRight,this)
+    this.zoneRight.on('pointerout',this.releaseRight,this)
+  }
+
+  holdLeft(){
+    if (!this.allowInput) return
+    if (this.isPause||this.isGameover) return
+    this.isHolding.left=true
+    this.isHolding.direction='left'
+        
+  }
+  holdRight(){
+    if (!this.allowInput) return
+    if (this.isPause||this.isGameover) return
+    this.isHolding.right=true
+    this.isHolding.direction='right'
+        
+  }
+
+  releaseLeft(){
+    this.isHolding.left=false
+    if (this.isHolding.right) {
+      this.isHolding.direction='right'
+      
+    }else{
+      this.isHolding.direction=false
+    }
+  }
+
+  releaseRight(){
+    this.isHolding.right=false
+    if (this.isHolding.left) {
+      this.isHolding.direction='left'
+      
+    }else{
+      this.isHolding.direction=false
+    }
   }
 }
 
